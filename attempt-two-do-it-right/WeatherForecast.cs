636379f3bd53472715fc2f279c6cbae62a90feba
Bailey Jackson <baj06080@vsc.edu>
2022-05-25 12:18:26 -0400

    whatever

---
+++ attempt-two-do-it-right/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace attempt_two_do_it_right
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
